77fbec4dd9354791400394ac269ffdde4adc80af
Phill <50775241+Phill030@users.noreply.github.com>
2025-11-06 13:02:25 +0100

    tests: update environment

--- tests/test_parser.cpp
+++ tests/test_parser.cpp
@@ -52,41 +52,13 @@ TEST(ParseFile, CanParseFile) {
 
 TEST(ParseFile, CanParseFile2) {
     std::string testFilePath = "data/WC_Z00_Hub.nif";
-
-    std::ifstream file(testFilePath, std::ios::binary);
-    if (!file.is_open())
-        throw std::runtime_error("Failed to open file: " + testFilePath);
-
-    file.seekg(0, std::ios::end);
-    std::streamsize size = file.tellg();
-    file.seekg(0, std::ios::beg);
-
-    std::vector<uint8_t> buffer(size);
-    if (!file.read(reinterpret_cast<char*>(buffer.data()), size))
-        throw std::runtime_error("Failed to read file: " + testFilePath);
-
-    NiFile niFile(buffer);
-
+    NiFile niFile(testFilePath);
     SUCCEED();
 }
 
 
 TEST(ParseFile, CanParseFile3) {
     std::string testFilePath = "data/WC_Unicorn_DSigns_A01_KH.nif";
-
-    std::ifstream file(testFilePath, std::ios::binary);
-    if (!file.is_open())
-        throw std::runtime_error("Failed to open file: " + testFilePath);
-
-    file.seekg(0, std::ios::end);
-    std::streamsize size = file.tellg();
-    file.seekg(0, std::ios::beg);
-
-    std::vector<uint8_t> buffer(size);
-    if (!file.read(reinterpret_cast<char*>(buffer.data()), size))
-        throw std::runtime_error("Failed to read file: " + testFilePath);
-
-    NiFile niFile(buffer);
-
+    NiFile niFile(testFilePath);
     SUCCEED();
 }
